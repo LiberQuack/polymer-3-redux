ec4caa72a6193a298dd9491aa469736fcc948fbb
Thiago Martins <tmartins@coddera.com>
2017-12-14 14:30:05 -0200

    Changed project language to typescript

--- src/state/store.ts
+++ src/state/store.ts
@@ -7,9 +7,7 @@ const reducers = combineReducers({
 
 const store = createStore(
     reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
 );
-
-window.applicationState = store;
 
 export { store as appState };
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,62 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
 
-    entry: './src/index.js',
+    entry: './src/index.ts',
 
     devServer: {
         contentBase: './src',
         historyApiFallback: true,
-        port: 7000,
+        port: 7001,
         inline: true,
         noInfo: false,
         host: '0.0.0.0',
         disableHostCheck: true
     },
 
+    devtool: "source-map",
+
     module: {
         loaders: [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-            }
-        },{
+            test: /\.svg$/,
+            loader: ['svg-sprite-loader'],
+        }, {
             test: /\.html$/,
             use: ['raw-loader']
-        },{
+        }, {
+            test: /(\.ts(x?)$)/,
+            exclude: /node_modules/,
+            use: [
+                {loader: 'ts-loader'}
+            ]
+        }, {
+            test: /\.(gif|png|jpe?g)$/i,
+            use: [
+                'file-loader',
+                {
+                    loader: 'image-webpack-loader',
+                    options: {
+                        bypassOnDebug: true,
+                    },
+                },
+            ],
+        }, {
             test: /\.scss$/,
-            use: ["style-loader", "css-loader", "sass-loader"]
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ["css-loader?sourceMap=true", "resolve-url-loader", "sass-loader?sourceMap=true"]
+            })
         }]
+    },
+
+    plugins: [
+        new ExtractTextPlugin("styles.css")
+    ],
+
+    resolve: {
+        extensions: ['.ts', '.js'],
+        modules: ['src', 'node_modules'],
     },
 
     output: {
